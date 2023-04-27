7026505d2c54769103622af2d0407209551bee2a
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-04-27 16:53:39 -0300

    git commit -m 'iniciando o projeto job-insights'

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -4,19 +4,9 @@ from typing import List, Dict
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+     with open(path, encoding="utf-8") as file:
+        return csv.DictReader(file)
+        
 
 
 def get_unique_job_types(path: str) -> List[str]:
