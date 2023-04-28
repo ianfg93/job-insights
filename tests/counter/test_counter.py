9d5c7b2c15e16965589f86105e784870c792886e
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-04-28 15:14:02 -0300

    feat: req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "data/jobs.csv"
+word_jobs = "jobs"
+word_python = "python"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, word_jobs) == 306
+    assert count_ocurrences(path, word_python) == 1639
