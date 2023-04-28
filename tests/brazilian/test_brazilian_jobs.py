b4e87c88a0ed618bacae6587d3dfa32c0508f1f3
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-04-28 15:45:35 -0300

    feat: req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    for key_words in read_brazilian_file(path):
+        assert "title" in key_words
+        assert "salary" in key_words
+        assert "type" in key_words
