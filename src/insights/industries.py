6ba1beff681db0a536ec39d8490024b467d1a5cf
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-04-28 12:45:44 -0300

    feat: req industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,15 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    set_industry = set()
+    for industry in jobs:
+        if industry["industry"] != "":
+            set_industry.add(industry["industry"])
+    return set_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [indust for indust in jobs if indust["industry"] == industry]
